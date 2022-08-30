f704e97d265ccb7a726e79871f06b30c2a503924
Gerrit <info@gerritschwerthelm.de>
2022-08-30 16:20:52 +0200

    Do power reset on machine deletion, not cycle.
    We don't need a graceful shutdown here, which delays the machine going back into our pool.

--- internal/bmc/nsq.go
+++ internal/bmc/nsq.go
@@ -64,7 +64,7 @@ func (b *BMCService) InitConsumer() error {
 				if err != nil {
 					return err
 				}
-				return outBand.PowerCycle()
+				return outBand.PowerReset()
 			case Command:
 				switch event.Cmd.Command {
 				case MachineOnCmd:
